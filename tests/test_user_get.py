3e6e678bc78e16681f933745e90b00fd88fab0b6
Kirill Napitkin <napitkin@gmail.com>
2024-11-14 12:54:27 +0300

    Ex16: Запрос данных другого пользователя

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -23,10 +23,36 @@ class TestUserGet(BaseCase):
         token = self.get_header(response1, "x-csrf-token")
         user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
-        response2 = MyRequests.get(f"/user/{user_id_from_auth_method}",
-                                 headers={"x-csrf-token": token},
-                                 cookies={"auth_sid": auth_sid},
+        response2 = MyRequests.get(
+            f"/user/{user_id_from_auth_method}",
+                headers={"x-csrf-token": token},
+                cookies={"auth_sid": auth_sid},
         )
 
         expected_fields = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+    # EX16: Запрос данных другого пользователя
+    def test_get_user_details_auth_as_other_user(self):
+        # Авторизация первым пользователем
+        data = {
+            "email": "vinkotov@example.com",
+            "password": "1234"
+        }
+        response1 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        # Запрашиваем данные второго пользователя
+        other_user_id = 3
+        response2 = MyRequests.get(
+            f"/user/{other_user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_json_has_key(response2, "username")
+        Assertions.assert_json_has_not_key(response2, "email")
+        Assertions.assert_json_has_not_key(response2, "firstName")
+        Assertions.assert_json_has_not_key(response2, "lastName")

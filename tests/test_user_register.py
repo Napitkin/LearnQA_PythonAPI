bb0e8d720e5cbd4cde6d8dd43321f4b5f87217d7
Kirill Napitkin <napitkin@gmail.com>
2024-11-14 12:39:35 +0300

    Ex15: Тесты на метод user

---
+++ tests/test_user_register.py
@@ -0,0 +1,64 @@
+import pytest
+from LearnQA_Python_API.lib.base_case import BaseCase
+from LearnQA_Python_API.lib.assertions import Assertions
+from LearnQA_Python_API.lib.my_requests import MyRequests
+
+
+class TestUserRegister(BaseCase):
+    def test_create_user_successfully(self):
+        data = self.prepare_registration_data()
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+
+    def test_create_user_with_existing_email(self):
+        email = "vinkotov@example.com"
+        data = self.prepare_registration_data(email)
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
+
+
+    def test_create_user_with_invalid_email(self):
+        email = "vinkotovexample.com"
+        data = self.prepare_registration_data(email)
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == "Invalid email format", f"Unexpected response content {response.content}"
+
+
+    @pytest.mark.parametrize("delete_field", ["password", "username", "firstName", "lastName", "email"])
+    def test_create_user_without_mandatory_field(self, delete_field):
+        data = self.prepare_registration_data()
+        data.pop(delete_field)  # Удаление одного обязательного поля
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"The following required params are missed: {delete_field}", f"Unexpected response content {response.content}"
+
+
+    def test_create_user_with_short_username(self):
+        data = self.prepare_registration_data()
+        data["username"] = "a"  # Создание пользователя с очень коротким именем в один символ
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == "The value of 'username' field is too short", f"Unexpected response content {response.content}"
+
+
+    def test_create_user_with_long_username(self):
+        data = self.prepare_registration_data()
+        data["username"] = "a" * 251  # Создание пользователя с очень длинным именем - длиннее 250 символов
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == "The value of 'username' field is too long", f"Unexpected response content {response.content}"
